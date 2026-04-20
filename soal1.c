e7c01c7b58520dbc477f0b8f7c22e4c24a6b0632
Marcho Ginting <march.gintings@gmail.com>
2026-04-20 09:36:32 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -64,7 +64,9 @@ int find_left(int *data, int size, int index) {
             index_left = find_left(data, size, index_corrupt);
             index_right = find_right(data, size, index_corrupt);
             if (index_left != -1 && index_right != -1) {
-                data[index_corrupt] = floor((data[index_left] + data[index_right]) / 2); // Floored
+                data[index_corrupt] = (data[index_left] + data[index_right]) / 2; // Floored
+                if ((data[index_left] + data[index_right]) < 0 && (data[index_left] % data[index_right]) != 0)
+                    data[index_corrupt] -= 1;
             } else if (index_left == -1 && index_right != -1) {
                 data[index_corrupt] = data[index_right];
             } else if (index_left != -1 && index_right == -1) {
